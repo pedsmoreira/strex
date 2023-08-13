63db47f7262f78bf32ecb17c779bac6688897004
Pedro Moreira <pedsmoreira@gmail.com>
2023-08-13 16:16:06 -0300

    Improve tests

--- src/match/match-text.test.ts
+++ src/match/match-text.test.ts
@@ -1,69 +1,100 @@
-import { expect, it } from "vitest";
+import { describe, expect, it } from "vitest";
 import { matchText } from "./match-text";
 
-it("matches case #1", () => {
-	expect(
-		matchText({
-			needle: ` from '`,
-			haystack: `import React from 'react';`,
-			mustMatchAtLineStart: false,
-			mustMatchAtLineEnd: false,
-		}),
-	).toEqual({
-		startColumnIndex: 12,
-		endColumnIndex: 19,
+describe("match anywhere", () => {
+	it("matches at the start", () => {
+		expect(
+			matchText({
+				needle: "hello",
+				haystack: "hello world from the moon",
+				mustMatchAtLineStart: false,
+				mustMatchAtLineEnd: false,
+			}),
+		).toEqual({
+			startColumnIndex: 0,
+			endColumnIndex: 5,
+		});
+	});
+
+	it("matches in the middle", () => {
+		expect(
+			matchText({
+				needle: " from ",
+				haystack: "hello world from the moon",
+				mustMatchAtLineStart: false,
+				mustMatchAtLineEnd: false,
+			}),
+		).toEqual({
+			startColumnIndex: 11,
+			endColumnIndex: 17,
+		});
+	});
+
+	it("matches at the end", () => {
+		expect(
+			matchText({
+				needle: "moon",
+				haystack: "hello world from the moon",
+				mustMatchAtLineStart: false,
+				mustMatchAtLineEnd: true,
+			}),
+		).toEqual({
+			startColumnIndex: 21,
+			endColumnIndex: 25,
+		});
 	});
 });
 
-it("matches case #2", () => {
-	expect(
-		matchText({
-			needle: "import ",
-			haystack: `import React from 'react';`,
-			mustMatchAtLineStart: false,
-			mustMatchAtLineEnd: false,
-		}),
-	).toEqual({
-		startColumnIndex: 0,
-		endColumnIndex: 7,
+describe("must match at line start", () => {
+	it("matches text at the start", () => {
+		expect(
+			matchText({
+				needle: "hello",
+				haystack: "hello world from the moon",
+				mustMatchAtLineStart: true,
+				mustMatchAtLineEnd: false,
+			}),
+		).toEqual({
+			startColumnIndex: 0,
+			endColumnIndex: 5,
+		});
+	});
+
+	it("does not match text is present but not at the start", () => {
+		expect(
+			matchText({
+				needle: " from ",
+				haystack: "hello world from the moon",
+				mustMatchAtLineStart: true,
+				mustMatchAtLineEnd: false,
+			}),
+		).toBeUndefined();
 	});
 });
 
-it("matches case #3", () => {
-	expect(
-		matchText({
-			needle: ` from '`,
-			haystack: `import React from 'react';`,
-			mustMatchAtLineStart: true,
-			mustMatchAtLineEnd: false,
-		}),
-	).toBeUndefined();
-});
-
-it("matches case #4", () => {
-	expect(
-		matchText({
-			needle: "import ",
-			haystack: `import React from 'react';`,
-			mustMatchAtLineStart: true,
-			mustMatchAtLineEnd: false,
-		}),
-	).toEqual({
-		startColumnIndex: 0,
-		endColumnIndex: 7,
+describe("must match at line end", () => {
+	it("matches text at the end", () => {
+		expect(
+			matchText({
+				needle: "moon",
+				haystack: "hello world from the moon",
+				mustMatchAtLineStart: false,
+				mustMatchAtLineEnd: true,
+			}),
+		).toEqual({
+			startColumnIndex: 21,
+			endColumnIndex: 25,
+		});
 	});
-});
 
-it("matches case #5", () => {
-	expect(
-		matchText({
-			needle: `';`,
-			haystack: `import React from 'react';`,
-			mustMatchAtLineStart: false,
-			mustMatchAtLineEnd: true,
-		}),
-	).toEqual({
-		startColumnIndex: 24,
-		endColumnIndex: 26,
+	it("does not match if text is not at the end", () => {
+		expect(
+			matchText({
+				needle: "hello",
+				haystack: "hello world from the moon",
+				mustMatchAtLineStart: false,
+				mustMatchAtLineEnd: true,
+			}),
+		).toBeUndefined();
 	});
 });
--- src/match/match-tuple-in-lines.test.ts
+++ src/match/match-tuple-in-lines.test.ts
@@ -4,18 +4,39 @@ import { matchTupleInLines } from "./match-tuple-in-lines";
 it("returns match parts given a tuple with text only", () => {
 	expect(
 		matchTupleInLines({
-			lines: [`import oneReducer from '../redux/one';`],
-			tuple: [undefined, { type: "text", text: "import " }],
+			lines: ["hello world from the moon"],
+			tuple: [undefined, { type: "text", text: "hello " }],
 			mustMatchAtLineStart: true,
 			mustMatchAtLineEnd: false,
 		}),
 	).toEqual([
 		{
 			type: "text",
-			text: "import ",
+			text: "hello ",
 			lineIndex: 0,
 			startColumnIndex: 0,
-			endColumnIndex: 7,
+			endColumnIndex: 6,
+		},
+	]);
+});
+
+it("returns match parts given a tuple with variable only", () => {
+	expect(
+		matchTupleInLines({
+			lines: ["hello world from the moon"],
+			tuple: [{ type: "variable", name: "remainder" }, undefined],
+			mustMatchAtLineStart: true,
+			mustMatchAtLineEnd: false,
+		}),
+	).toEqual([
+		{
+			type: "variable",
+			name: "remainder",
+			value: "hello world from the moon",
+			startLineIndex: 0,
+			endLineIndex: 0,
+			startColumnIndex: 0,
+			endColumnIndex: 25,
 		},
 	]);
 });
@@ -23,10 +44,10 @@ it("returns match parts given a tuple with text only", () => {
 it("returns match parts given a tuple with variable + text", () => {
 	expect(
 		matchTupleInLines({
-			lines: [`oneReducer from '../redux/one';`],
+			lines: ["hello world from the moon"],
 			tuple: [
-				{ type: "variable", name: "imports" },
-				{ type: "text", text: " from '" },
+				{ type: "variable", name: "what" },
+				{ type: "text", text: " from " },
 			],
 			mustMatchAtLineStart: true,
 			mustMatchAtLineEnd: false,
@@ -34,18 +55,18 @@ it("returns match parts given a tuple with variable + text", () => {
 	).toEqual([
 		{
 			type: "variable",
-			name: "imports",
-			value: "oneReducer",
+			name: "what",
+			value: "hello world",
 			startLineIndex: 0,
 			startColumnIndex: 0,
 			endLineIndex: 0,
-			endColumnIndex: 10,
+			endColumnIndex: 11,
 		},
 		{
 			type: "text",
-			text: " from '",
+			text: " from ",
 			lineIndex: 0,
-			startColumnIndex: 10,
+			startColumnIndex: 11,
 			endColumnIndex: 17,
 		},
 	]);
